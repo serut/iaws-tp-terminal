11e15aa9e3ef5c6881c1c00b1ff7d5b7c3cfb185
manantsoa <manantsoa.razana@gmail.com>
2015-03-18 11:38:08 +0100

    new class Film

---
+++ src/main/java/fr/ups/dl/iaws/Film.java
@@ -0,0 +1,40 @@
+package fr.ups.dl.iaws;
+
+/**
+ * Created by manantsoa on 18/03/15.
+ */
+public class Film {
+    String id;
+    String title;
+    int year;
+
+    public Film(String id, String title, int year) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
