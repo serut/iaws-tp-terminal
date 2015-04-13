2c2035afd6434ec0652c2e60cdb1deb45aedc378
manantsoa <manantsoa.razana@gmail.com>
2015-04-13 18:53:08 +0200

    Retourner des salles avec du SOAP (pas de tests)

---
+++ src/main/java/fr/ups/dl/iaws/contractfirst/CinemaEndPoint.java
@@ -0,0 +1,36 @@
+package fr.ups.dl.iaws.contractfirst;
+
+import fr.ups.dl.iaws.model.Salle;
+import fr.ups.dl.iaws.model.Salles;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.springframework.ws.server.endpoint.annotation.XPathParam;
+
+import javax.xml.bind.Element;
+import java.util.List;
+
+/**
+ * Created by manantsoa on 13/04/15.
+ */
+@Endpoint
+public class CinemaEndPoint {
+
+        private List<Salle> salles;
+        private Salles serviceSalle;
+
+        private static final String NAMESPACE_URI = "http://fr/ups/dl/iaws/contractfirst/results";
+
+        @PayloadRoot(localPart="CinemaRequest", namespace = NAMESPACE_URI)
+        @ResponsePayload
+        public void handleCinemaRequest(@XPathParam("/CinemaRequest/id") int id) throws Exception {
+
+            salles =  serviceSalle.getSallesFilm(String.valueOf(id));
+
+            /*Element elt = XmlHelper.getRootElementFromFileInClasspath("ReleveNotes.xml") ;
+            return  elt;*/
+            System.out.println(id);
+        }
+
+
+}
